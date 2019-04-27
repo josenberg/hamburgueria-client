6ab9e3aa3edeb34c3a69f6b42b5d31574b4784fd
jpg <mbx.philipe@gmail.com>
2019-04-26 22:58:44 -0300

    Configurations for babel, webpack and first index

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackDash = require('webpack-dashboard/plugin');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'app');
+
+const paths = {
+  source: path.resolve(__dirname, './app'),
+  build: path.resolve(__dirname, './public'),
+};
+
+const config = {
+  entry: `${APP_DIR}/main.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR,
+    publicPath: '/',
+  },
+  plugins: [
+    new WebpackDash(),
+    // new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(paths.build, 'template.html'),
+      filename: path.join(paths.build, 'index.html'),
+    }),
+  ],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env'],
+        },
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name]-[hash:8].[ext]',
+            },
+          }
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
