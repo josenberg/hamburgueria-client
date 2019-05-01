6f29cf0b10deefab0e441a3d07ca253a8e89b4d0
jpg <mbx.philipe@gmail.com>
2019-05-01 14:17:06 -0300

    Adding testing for when menuSagas failed

--- test/state/menu/sagas.test.jsx
+++ test/state/menu/sagas.test.jsx
@@ -2,22 +2,32 @@ import 'babel-polyfill';
 import { call, put } from 'redux-saga/effects';
 
 import { fetchMenu } from '../../../app/state/menu/sagas';
-import { fetchMenuSuccess } from '../../../app/state/menu/actions';
+import {fetchMenuSuccess, fetchMenuFailure } from '../../../app/state/menu/actions';
 import * as API from '../../../app/api';
 
-jest.mock('../../../app/api', () => ({
-  fetchMenu: () => ([{
-    id: 1,
-    name: 'xbacon',
-    displayName: 'X-Bacon',
-    ingredients: [2, 3, 5],
-  }]),
-}));
-
 describe('Sagas Reducer', () => {
-  it('should return the initial state', () => {
+  it('should call the fetchMenu and send its results to the fetchMenuSuccess', () => {
+    jest.mock('../../../app/api', () => ({
+      fetchMenu: () => ([{
+        id: 1,
+        name: 'xbacon',
+        displayName: 'X-Bacon',
+        ingredients: [2, 3, 5],
+      }]),
+    }));
     const gen = fetchMenu();
     expect(gen.next().value).toEqual(call(API.fetchMenu));
     expect(gen.next().value).toEqual(put(fetchMenuSuccess()));
+  });
+
+  it('It should call the fetchMenuFailed in case of error', () => {
+    jest.mock('../../../app/api', () => {
+      throw new Error('Error, error, Errror!');
+    });
+    const gen = fetchMenu();
+
+    expect(gen.next().value).toEqual(call(API.fetchMenu));
+    expect(gen.throw('Generic error').value)
+      .toEqual(put(fetchMenuFailure('Generic error')));
   });
 });
